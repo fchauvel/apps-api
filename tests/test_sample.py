1be851b9d0ab61983c8f0f50ff64bebec544ba5c
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2023-08-03 15:28:07 +0800

    feature: Sample API test

---
+++ tests/test_sample.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from api.runner import app
+from fastapi.testclient import TestClient
+
+
+class SampleTests(TestCase):
+
+    def setUp(self):
+        self.client = TestClient(app)
+
+    def test_works(self):
+        response = self.client.get("/test")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual({"message": "Hello World"},
+                           response.json())
