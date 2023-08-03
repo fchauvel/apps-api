8c52959a8d49a2e6cdf96caf176cfc7d1ed9ed03
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2023-08-03 15:04:08 +0800

    feature: Dummy '/test' endpoint

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apps-api",
+    version="0.0.1",
+    url="https://github.com/fchauvel/apps-api.git",
+    author="Franck Chauvel",
+    author_email="franck.chauvel@gmail.com",
+    description="Web API for Apps",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn"
+    ],
+)
